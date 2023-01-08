0bd5d424950348dead5f7cc15eaa6f5524e5d4b1
archieval <Syarchieval@gmail.com>
2023-01-09 01:22:02 +0800

    fixing missing dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Suspense, useEffect, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /* Pges */
-// import AdminContainer from "./Pages/Admin/AdminContainer";
+import AdminContainer from "./Pages/Admin/AdminContainer";
 import NotFound from "./Pages/NotFound";
 import AuthContainer from "./Pages/Auth/AuthContainer";
 import AccountProfile from "./Pages/User/AccountProfile";
@@ -26,9 +26,64 @@ import { signin } from "./Features/userSlice";
 import { adminSign } from "./Features/adminSlice";
 import FullPageLoader from "./Components/ProtectedLoader";
 
+const PublicContainer = lazy(() => import("./Pages/Public/PublicContainer"));
+
 function App() {
+  const dispatch = useDispatch();
+
+  const checkIfUserIsSaved = async () => {
+    let savedUser = JSON.parse(localStorage.getItem("userData"));
+    let savedAdmin = JSON.parse(localStorage.getItem("adminData"));
+
+    if (!savedUser && !savedAdmin) return;
+
+    try {
+      if (savedUser) {
+        getAuth()
+        const userResponse = await API.post(`/user/mydetails/${savedUser._id}`, { auth: getAuth() });
+        dispatch(signin(userResponse.data.userData));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      if (savedAdmin) {
+        const adminResponse = await API.post(`/admin/mydetails/${savedAdmin._id}`, { auth: getAuth() });
+        dispatch(adminSign(adminResponse.data.adminData));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    checkIfUserIsSaved();
+  });
+
+  console.log("Load APP");
   return (
     <div className="App">
+      <Suspense fallback={<FullPageLoader />}>
+        <button className="btn bg-neutral-700"> yow</button>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <Router>
+          <Informative />
+          <InputModal />
+          <Notifier />
+          <Switch>
+            <Route path="/auth" component={AuthContainer} />
+            <Route path="/admin" component={AdminContainer} />
+            <ProtectedRoute path="/user" component={AccountProfile} />
+            <ProtectedRoute path="/account" component={AccountSettings} />
+
+            <Route path="/" component={PublicContainer} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Router>
+      </Suspense>
     </div>
   )
 }
